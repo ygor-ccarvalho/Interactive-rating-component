49c8ed391c2d63a716f392255cc804c340048273
Ygor-CCarvalho <ygorcdc@gmail.com>
2024-03-24 15:19:27 -0300

    projeto-finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,21 @@
 const rates = document.querySelectorAll(".option");
+const submit = document.getElementById("btn-submit");
+const firstPage = document.querySelector(".first-page");
+const thankYou = document.querySelector(".thank-you");
+const rateSelected = document.getElementById('rate-selected');
 
 rates.forEach( (rate) => {
     rate.addEventListener('click', () => {
-        console.log(rate.innerHTML)
+        rateSelected.innerHTML = rate.innerHTML;
     })    
-});    
+});
 
+
+
+submit.addEventListener('click', () => {
+    firstPage.classList.add('hide');
+    thankYou.classList.remove('hide');
+});
 
 
 
